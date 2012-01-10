892e7ae252aa0f3b233a26161f66199b0c91ae34
Izoria Vladislav <vladislav.izoria@gmail.com>
2012-01-10 18:38:17 +0400

    Fix value option

--- lib/formtastic_datepicker/hook.rb
+++ lib/formtastic_datepicker/hook.rb
@@ -9,6 +9,8 @@ module FormtasticDatepicker
         include FormtasticDatepicker::Helper
 
         def datepicker_input(method, options)
+          ActiveRecord::Base.logger.info options.to_yaml
+
           format = options[:format] || Date::DATE_FORMATS[:default] || '%d %b %Y'
           inp_opts = options.delete(:input_html) || {}
           inp_opts[:value] ||= object.send(method).try(:strftime, format)
@@ -18,7 +20,6 @@ module FormtasticDatepicker
           clss << options.delete(:class).to_s
 
           options[:input_html] = {:class => "datepicker " + clss.join(' ')}.merge inp_opts
-          ActiveRecord::Base.logger.info options.to_yaml
           basic_input_helper(:text_field, :string, method, options)
         end
       end
